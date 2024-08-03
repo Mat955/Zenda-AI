14e72d9fa2d21d5ddf551785148575b37143f920
Matt955 <lewar2013c@gmail.com>
2024-08-03 14:58:44 +0200

    refactor: Update appointment page and related components for improved performance and readability

--- src/app/(dashboard)/appointment/page.tsx
+++ src/app/(dashboard)/appointment/page.tsx
@@ -7,22 +7,22 @@ import React from 'react';
 type Props = {};
 
 const Page = async (props: Props) => {
-  const user = await currentUser();
+	const user = await currentUser();
 
-  if (!user) return null;
-  const domainBookings = await onGetAllBookingsForCurrentUser(user.id);
-  const today = new Date();
+	if (!user) return null;
+	const domainBookings = await onGetAllBookingsForCurrentUser(user.id);
+	const today = new Date();
 
-  return (
-    <>
-      <InfoBar />
-      <div className="grid grid-cols-1 lg:grid-cols-3 flex-1 h-0 gap-5">
-        <div className="lg:col-span-2 overflow-y-auto">
-          <AllAppointments bookings={domainBookings?.bookings} />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<InfoBar />
+			<div className="grid grid-cols-1 lg:grid-cols-3 flex-1 h-0 gap-5">
+				<div className="lg:col-span-2 overflow-y-auto">
+					<AllAppointments bookings={domainBookings?.bookings} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Page;
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from '../ui/table';
 import { cn } from '@/lib/utils';
 
 type DataTableProps = {
-  headers: string[];
-  children: React.ReactNode;
+	headers: string[];
+	children: React.ReactNode;
 };
 
 const DataTable = ({ headers, children }: DataTableProps) => {
-  return (
-    <Table className="rounded-t-xl overflow-hidden">
-      <TableHeader>
-        <TableRow className="bg-grandis">
-          {headers.map((header, key) => (
-            <TableHead
-              key={key}
-              className={cn(key == headers.length - 1 && 'text-right')}
-            >
-              {header}
-            </TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>{children}</TableBody>
-    </Table>
-  );
+	return (
+		<Table className="rounded-t-xl overflow-hidden">
+			<TableHeader>
+				<TableRow className="bg-grandis">
+					{headers.map((header, key) => (
+						<TableHead
+							key={key}
+							className={cn(
+								key == headers.length - 1 && 'text-right',
+								'text-black',
+							)}
+						>
+							{header}
+						</TableHead>
+					))}
+				</TableRow>
+			</TableHeader>
+			<TableBody>{children}</TableBody>
+		</Table>
+	);
 };
 
 export default DataTable;
