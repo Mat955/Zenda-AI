d374f34292df14cb4d41add7534ca56de52b7747
Mateusz Lewartowski <lewar2013c@gmail.com>
2024-08-23 15:12:30 +0200

    refactor: Update landing page banner color per client request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import NavBar from '@/components/navbar';
 import { Button } from '@/components/ui/button';
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { pricingCards } from '@/constants/landing-page';
 import clsx from 'clsx';
@@ -19,88 +19,85 @@ import React from 'react';
 type Props = {};
 
 const Home = (props: Props) => {
-	return (
-		<main className="bg-white">
-			<NavBar />
-			<section>
-				<div className="flex items-center justify-center flex-col mt-[80px] gap-4">
-					<span className="text-orange bg-orange/20 px-4 py-2 rounded-full text-sm">
-						An AI powered sales assistant chatbot
-					</span>
-					<Image
-						src="/images/logo.png"
-						width={500}
-						height={100}
-						alt="logo"
-						className="max-w-lg object-contain"
-					/>
-					<p className="text-center max-w-[500px]">
-						Your AI powered sales assistant! Embed Zenda AI into any website
-						with just a snippet of code!
-					</p>
-					<Button className="bg-orange font-bold text-white px-4">
-						Start For Free
-					</Button>
-					<Image
-						src="/images/iphonecorinna.png"
-						width={400}
-						height={100}
-						alt="iphonecorinna logo"
-						className="max-w-lg object-contain"
-					/>
-				</div>
-			</section>
-			<section className="flex justify-center items-center flex-col gap-4 mt-10">
-				<h2 className="text-4xl text-center">Choose what fits you right</h2>
-				<p className="text-muted-foreground text-center max-w-lg">
-					Our straightforward pricing model is tailored to your needs. If
-					{"you're"} just starting out, we have a free plan for you.
-				</p>
-			</section>
-			<div className="flex justify-center gap-4 flex-wrap mt-6">
-				{pricingCards.map((card) => (
-					<Card
-						key={card.title}
-						className={clsx('w-[300px] flex flex-col justify-between', {
-							'border-2 border-primary': card.title === 'Ultimate',
-						})}
-					>
-						<CardHeader>
-							<CardTitle className="text-orange">{card.title}</CardTitle>
-							<CardDescription>
-								{pricingCards.find((c) => c.title === card.title)?.description}
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<span className="text-4xl font-bold">{card.price}</span>
-							<span className="text-muted-foreground">
-								<span>/ month</span>
-							</span>
-						</CardContent>
-						<CardFooter className="flex flex-col items-start gap-4">
-							<div>
-								{card.features.map((feature) => (
-									<div
-										key={feature}
-										className="flex gap-2"
-									>
-										<Check />
-										<p>{feature}</p>
-									</div>
-								))}
-							</div>
-							<Link
-								href={`/dashboard?plan=${card.title}`}
-								className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
-							>
-								Get Started
-							</Link>
-						</CardFooter>
-					</Card>
-				))}
-			</div>
-		</main>
-	);
+  return (
+    <main className="bg-white">
+      <NavBar />
+      <section>
+        <div className="flex items-center justify-center flex-col mt-[80px] gap-4">
+          <span className="text-orange bg-orange/20 px-4 py-2 rounded-full text-sm">
+            An AI powered sales assistant chatbot
+          </span>
+          <Image
+            src="/images/logo.png"
+            width={500}
+            height={100}
+            alt="logo"
+            className="max-w-lg object-contain"
+          />
+          <p className="text-center max-w-[500px]">
+            Your AI powered sales assistant! Embed Zenda AI into any website
+            with just a snippet of code!
+          </p>
+          <Button className="bg-orange font-bold text-white px-4">
+            Start For Free
+          </Button>
+          <Image
+            src="/images/iphonecorinna.png"
+            width={400}
+            height={100}
+            alt="iphonecorinna logo"
+            className="max-w-lg object-contain"
+          />
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 mt-10">
+        <h2 className="text-4xl text-center">Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center max-w-lg">
+          Our straightforward pricing model is tailored to your needs. If
+          {"you're"} just starting out, we have a free plan for you.
+        </p>
+      </section>
+      <div className="flex justify-center gap-4 flex-wrap mt-6">
+        {pricingCards.map((card) => (
+          <Card
+            key={card.title}
+            className={clsx('w-[300px] flex flex-col justify-between', {
+              'border-2 border-primary': card.title === 'Ultimate',
+            })}
+          >
+            <CardHeader>
+              <CardTitle className="text-orange">{card.title}</CardTitle>
+              <CardDescription>
+                {pricingCards.find((c) => c.title === card.title)?.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">{card.price}</span>
+              <span className="text-muted-foreground">
+                <span>/ month</span>
+              </span>
+            </CardContent>
+            <CardFooter className="flex flex-col items-start gap-4">
+              <div>
+                {card.features.map((feature) => (
+                  <div key={feature} className="flex gap-2">
+                    <Check />
+                    <p>{feature}</p>
+                  </div>
+                ))}
+              </div>
+              <Link
+                href={`/dashboard?plan=${card.title}`}
+                className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
+              >
+                Get Started
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default Home;
