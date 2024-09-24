86a8878b781ec07bc0e396ac91984469c102dcbc
Mateusz Lewartowski <lewar2013c@gmail.com>
2024-09-24 18:30:42 +0200

    Update middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { authMiddleware } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'],
@@ -9,8 +8,3 @@ export default authMiddleware({
 export const config = {
   matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-export function middleware(request) {
-  // Add your middleware logic here
-  return NextResponse.next();
-}
