aa530c8419124d299fe354a79a87ed4a95be611e
Mateusz Lewartowski <lewar2013c@gmail.com>
2024-09-24 14:11:14 +0200

    refactor: Add dashboard components for displaying user metrics and usage
    - Added new components for displaying user metrics and usage on the dashboard page.
    - Created the DashboardCard component for rendering individual metric cards.
    - Created the PlanUsage component for displaying plan usage progress bars.
    - Added the ProgressBar component for rendering progress bars.
    - Updated the Dashboard page to fetch and display user metrics and usage data.

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,79 @@
+import { getUserAppointments } from '@/actions/appointment';
+import {
+  getUserBalance,
+  getUserClients,
+  getUserPlanInfo,
+  getUserTotalProductPrices,
+  getUserTransactions,
+} from '@/actions/dashboard';
+import DashboardCard from '@/components/dashboard/cards';
+import PlanUsage from '@/components/dashboard/plan-usage';
+import InfoBar from '@/components/infobar';
+import CalIcon from '@/icons/cal-icon';
+import EmailIcon from '@/icons/email-icon';
+import MoneyIcon from '@/icons/money-icon';
+import PersonIcon from '@/icons/person-icon';
+import { CalendarIcon, DollarSignIcon } from 'lucide-react';
 import React from 'react';
 
 type Props = {};
 
-const Dashboard = (props: Props) => {
-  return <div>Dashboard</div>;
+const Dashboard = async (props: Props) => {
+  const clients = await getUserClients();
+  const sales = await getUserBalance();
+  const appointments = await getUserAppointments();
+  const plan = await getUserPlanInfo();
+  const transactions = await getUserTransactions();
+  const products = await getUserTotalProductPrices();
+
+  return (
+    <>
+      <InfoBar />
+      <div className="overflow-y-auto w-full chat-window flex-1 h-0">
+        <div className="flex gap-5 flex-wrap">
+          <DashboardCard
+            value={clients || 0}
+            title="Potential Clients"
+            icon={<PersonIcon />}
+          />
+          <DashboardCard
+            value={products! * clients! || 0}
+            sales
+            title="Pipline Value"
+            icon={<MoneyIcon />}
+          />
+          <DashboardCard
+            value={appointments || 0}
+            title="Appointments"
+            icon={<CalIcon />}
+          />
+          <DashboardCard
+            value={sales || 0}
+            sales
+            title="Total Sales"
+            icon={<DollarSignIcon />}
+          />
+        </div>
+        <div className="w-full grid grid-cols-1 lg:grid-cols-2 py-10">
+          <div>
+            <div>
+              <h2 className="font-bold text-2xl">Plan Usage</h2>
+              <p className="text-sm font-light">
+                A detailed overview of your metrics, usage and more data.
+              </p>
+            </div>
+            <PlanUsage
+              plan={plan?.plan}
+              credits={plan?.credits || 0}
+              domains={plan?.domains || 0}
+              clients={clients || 0}
+            />
+          </div>
+          <div className="flex flex-col"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/progress/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Progress } from '../ui/progress';
+
+type ProgressBarProps = {
+  label: string;
+  end: number;
+  credits: number;
+};
+
+const ProgressBar = ({ label, end, credits }: ProgressBarProps) => {
+  return (
+    <div className="flex flex-col w-full md-w-7/12 gap-1">
+      <h2 className="font-bold">{label}</h2>
+      <div className="flex flex-col">
+        <div className="flex justify-between text-sm">
+          <p>{credits}</p>
+          <p>{end}</p>
+        </div>
+        <Progress value={(credits / end) * 100} className="w-full" />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
