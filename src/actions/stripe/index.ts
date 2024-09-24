29c20989182dde563270e2acdbd7ed89d627c603
Mateusz Lewartowski <lewar2013c@gmail.com>
2024-09-24 16:08:09 +0200

    Refactor apiversion && realtime mode

--- src/actions/stripe/index.ts
+++ src/actions/stripe/index.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET!, {
   typescript: true,
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
 });
 
 export const onCreateCustomerPaymentIntentSecret = async (
